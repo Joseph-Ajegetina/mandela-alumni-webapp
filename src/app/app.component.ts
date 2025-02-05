85ff5fa104680f9e0d2a6363299b29afd6df929c
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-02-05 11:47:09 +0000

    add setup for taiga ui with core kit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { TuiAlertService, TuiButton, TuiRoot } from '@taiga-ui/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-	imports: [RouterModule],
+	imports: [RouterModule, TuiRoot, TuiButton],
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.less',
 })
 export class AppComponent {
 	title = 'mandela-alumni-webapp';
+
+	private readonly alerts = inject(TuiAlertService);
+
+	protected showNotification(): void {
+		this.alerts.open('Basic <strong>HTML</strong>', { label: 'With a heading!' }).subscribe();
+	}
 }
