3d60f953547e04d0259abf9340228d4d9c03a646
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-02-16 19:40:35 +0000

    Add base template dashboard skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/router';
 import { RegisterComponent } from './features/auth/feature/register/register.component';
 import { LoginComponent } from './features/auth/feature/login/login.component';
+import { LayoutComponent } from './shared/ui/layout/layout.component';
 
 export const appRoutes: Route[] = [
 	{
@@ -13,7 +14,12 @@ export const appRoutes: Route[] = [
 	},
 
 	{
+		path: 'dashboard',
+		component: LayoutComponent,
+	},
+
+	{
 		path: '**',
-		redirectTo: '/login',
+		redirectTo: '/register',
 	},
 ];
---
+++ src/app/shared/ui/layout/layout.component.ts
@@ -0,0 +1,83 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TuiNavigation } from '@taiga-ui/layout';
+import {
+	TuiButton,
+	TuiDataList,
+	TuiOptGroup,
+	TuiDropdownService,
+	TuiDropdown,
+	TuiTextfield,
+	TuiAppearance,
+	TuiLink,
+} from '@taiga-ui/core';
+import {
+	TuiAvatar,
+	TuiBadge,
+	TuiBreadcrumbs,
+	TuiChevron,
+	TuiDataListDropdownManager,
+	TuiFade,
+	TuiSwitch,
+	TuiTabs,
+} from '@taiga-ui/kit';
+import { tuiAsPortal, TuiItem, TuiPortals } from '@taiga-ui/cdk';
+import { ICON } from '../icon';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+	selector: 'app-layout',
+	imports: [
+		CommonModule,
+		FormsModule,
+		RouterLink,
+		RouterLinkActive,
+		TuiNavigation,
+		TuiButton,
+		TuiDataList,
+		TuiDataListDropdownManager,
+		TuiOptGroup,
+		TuiSwitch,
+		TuiChevron,
+		TuiFade,
+		TuiBadge,
+		TuiAvatar,
+		TuiDropdown,
+		TuiBreadcrumbs,
+		TuiTextfield,
+		TuiItem,
+		TuiAppearance,
+		TuiLink,
+		TuiTabs,
+	],
+	templateUrl: './layout.component.html',
+	styleUrl: './layout.component.less',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	providers: [TuiDropdownService, tuiAsPortal(TuiDropdownService)],
+})
+export class LayoutComponent extends TuiPortals {
+	protected expanded = signal(false);
+	protected open = false;
+	protected switch = false;
+	protected readonly routes: any = {};
+
+	protected readonly drawer = {
+		Components: [
+			{ name: 'Button', icon: ICON },
+			{ name: 'Input', icon: ICON },
+			{ name: 'Tooltip', icon: ICON },
+		],
+		Essentials: [
+			{ name: 'Getting started', icon: ICON },
+			{ name: 'Showcase', icon: ICON },
+			{ name: 'Typography', icon: ICON },
+		],
+	};
+
+	protected breadcrumbs = ['Home', 'Angular', 'repositories', 'taiga ui'];
+
+	protected handleExpandedToggle(): void {
+		this.expanded.update((value) => !value);
+	}
+}
