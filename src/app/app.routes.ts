fd680774a63385a0d0dda2d5d1ab9551ed8897a0
profe-ssor <kyerematengcollins93@gmail.com>
2025-02-17 20:13:06 +0000

    merging conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Route } from '@angular/router';
+import { TodoListComponent } from './Components/todo-list/todo-list.component';
+import { AddTodoComponent } from './Components/add-todo/add-todo.component';
+import { RegisterComponent } from './features/auth/feature/register/register.component';
+import { LoginComponent } from './features/auth/feature/login/login.component';
 
-export const appRoutes: Route[] = [];
+  
+
+
+export const appRoutes: Route[] = [
+	{
+		path: 'login',
+		component: LoginComponent,
+	},
+	{
+		path: 'register',
+		component: RegisterComponent,
+	},
+
+	{
+		path: '**',
+		redirectTo: '/login',
+	},
+
+    {
+        path: 'add-todo',
+        component: AddTodoComponent
+      },
+      {
+        path: 'todo-list',
+        component: TodoListComponent
+      },
+];
+
