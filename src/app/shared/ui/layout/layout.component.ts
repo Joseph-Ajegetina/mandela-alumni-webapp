69ca74cc252775a8bb0b4fac4d37d5a449c5a4c9
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-02-17 21:16:46 +0000

    Add toggle for theme selector

--- src/app/shared/ui/layout/layout.component.ts
+++ src/app/shared/ui/layout/layout.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiNavigation } from '@taiga-ui/layout';
+import { WA_LOCAL_STORAGE, WA_WINDOW } from '@ng-web-apis/common';
 import {
 	TuiButton,
 	TuiDataList,
@@ -10,6 +11,8 @@ import {
 	TuiTextfield,
 	TuiAppearance,
 	TuiLink,
+	TUI_DARK_MODE_KEY,
+	TUI_DARK_MODE,
 } from '@taiga-ui/core';
 import {
 	TuiAvatar,
@@ -21,7 +24,7 @@ import {
 	TuiSwitch,
 	TuiTabs,
 } from '@taiga-ui/kit';
-import { tuiAsPortal, TuiItem, TuiPortals } from '@taiga-ui/cdk';
+import { tuiAsPortal, TuiItem, TuiPortals, TuiThemeColorService } from '@taiga-ui/cdk';
 import { ICON } from '../icon';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
@@ -57,6 +60,15 @@ import { MenuItem } from '../../interfaces/menu-item';
 	providers: [TuiDropdownService, tuiAsPortal(TuiDropdownService)],
 })
 export class LayoutComponent extends TuiPortals {
+	protected key = inject(TUI_DARK_MODE_KEY);
+	protected storage = inject(WA_LOCAL_STORAGE);
+	protected media = inject(WA_WINDOW).matchMedia('(prefers-color-scheme: dark)');
+	protected darkMode = inject(TUI_DARK_MODE);
+
+	private readonly themeService = inject(TuiThemeColorService);
+
+	isDarkMode = computed(() => (this.darkMode() ? 'dark' : 'light'));
+
 	protected expanded = signal(false);
 	protected open = false;
 	protected switch = false;
