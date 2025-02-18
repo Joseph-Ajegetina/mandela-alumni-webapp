d3ca342e70b1c2425bb8edac7865cab9cc072ab1
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-02-18 00:45:29 +0000

    add theme selector

--- src/app/shared/ui/layout/layout.component.ts
+++ src/app/shared/ui/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiNavigation } from '@taiga-ui/layout';
-import { WA_LOCAL_STORAGE, WA_WINDOW } from '@ng-web-apis/common';
+import { WA_LOCAL_STORAGE } from '@ng-web-apis/common';
 import {
 	TuiButton,
 	TuiDataList,
@@ -11,7 +11,6 @@ import {
 	TuiTextfield,
 	TuiAppearance,
 	TuiLink,
-	TUI_DARK_MODE_KEY,
 	TUI_DARK_MODE,
 } from '@taiga-ui/core';
 import {
@@ -24,8 +23,7 @@ import {
 	TuiSwitch,
 	TuiTabs,
 } from '@taiga-ui/kit';
-import { tuiAsPortal, TuiItem, TuiPortals, TuiThemeColorService } from '@taiga-ui/cdk';
-import { ICON } from '../icon';
+import { tuiAsPortal, TuiItem, TuiPortals } from '@taiga-ui/cdk';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { MenuItem } from '../../interfaces/menu-item';
@@ -60,31 +58,16 @@ import { MenuItem } from '../../interfaces/menu-item';
 	providers: [TuiDropdownService, tuiAsPortal(TuiDropdownService)],
 })
 export class LayoutComponent extends TuiPortals {
-	protected key = inject(TUI_DARK_MODE_KEY);
-	protected storage = inject(WA_LOCAL_STORAGE);
-	protected media = inject(WA_WINDOW).matchMedia('(prefers-color-scheme: dark)');
 	protected darkMode = inject(TUI_DARK_MODE);
 
-	private readonly themeService = inject(TuiThemeColorService);
-
 	isDarkMode = computed(() => (this.darkMode() ? 'dark' : 'light'));
-
-	protected expanded = signal(false);
-	protected open = false;
-	protected switch = false;
-	protected readonly routes: any = {};
+	protected openDrawer = false;
 
 	protected readonly menuItems = signal<MenuItem[]>([
 		{
-			icon: '@tui.home',
+			icon: '@tui.layout',
 			label: 'Dashboard',
 			route: 'dashboard',
 		},
 	]);
-
-	protected breadcrumbs = ['Home', 'Angular', 'repositories', 'taiga ui'];
-
-	protected handleToggle(): void {
-		this.expanded.update((value) => !value);
-	}
 }
