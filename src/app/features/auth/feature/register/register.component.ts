57a5c99220507079a3bc4205951375ab8b3673fe
profe-ssor <kyerematengcollins93@gmail.com>
2025-02-21 11:31:20 +0000

    Register Page Done with Validations

--- src/app/features/auth/feature/register/register.component.ts
+++ src/app/features/auth/feature/register/register.component.ts
@@ -1,57 +1,67 @@
-import { Component, inject, input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RegisterFormComponent } from '../../ui/register-form/register-form.component';
-import {
-	FormBuilder,
-	FormControl,
-	FormGroup,
-	ReactiveFormsModule,
-	Validators,
-} from '@angular/forms';
-import { AuthService } from '../../data-access/auth.service';
-import { RegisterData } from '../../models/register-data';
-import { Roles } from '../../models/roles';
-import { User } from '../../models/user';
+import { Component} from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TuiButton, TuiDataList } from '@taiga-ui/core';
+import { TuiAvatar, TuiDataListWrapper } from '@taiga-ui/kit';
+import { TuiInputModule, TuiSelectModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+import { Constant } from 'src/app/shared/constant/contant';
+
+
 
 @Component({
 	selector: 'app-register',
-	imports: [CommonModule, RegisterFormComponent],
+	imports: [
+		ReactiveFormsModule, TuiInputModule, TuiButton, TuiAvatar, 
+		TuiTextfieldControllerModule,TuiSelectModule,  TuiDataList,  TuiDataListWrapper,],
 	templateUrl: './register.component.html',
-	styleUrl: './register.component.css',
+	styleUrl: './register.component.less',
 })
 export class RegisterComponent {
-	authService = inject(AuthService);
-	fb = inject(FormBuilder);
-	form!: FormGroup;
-
-	constructor() {
-		this.setupForm();
-	}
-
-	registerUser() {
-		const registrationDTO: RegisterData = this.getRegistrationDTO();
-		this.authService.registerUser(registrationDTO).subscribe((user: User) => {
-			console.log('user created');
-		});
-	}
-
-	getRegistrationDTO() {
-		const firstName = this.form.controls['firstName'].value;
-		const lastName = this.form.controls['lastName'].value;
-		const email = this.form.controls['email'].value;
-		const phone = this.form.controls['phone'].value;
-		const role = this.form.controls['role'].value;
-
-		return { firstName, lastName, email, phone, role };
-	}
-
-	private setupForm() {
-		this.form = this.fb.group({
-			firstName: ['', Validators.required],
-			lastName: ['', Validators.required],
-			phone: ['', [Validators.required, Validators.minLength(8)]],
-			email: ['', [Validators.email, Validators.required]],
-			role: ['member'],
-		});
-	}
+	constant = Constant
+	protected items = [
+	'Alumni',
+	'Member'
+	];
+	registerForm: FormGroup = new FormGroup({
+		firstName: new FormControl(
+			'', 
+			[
+				Validators.required,
+				Validators.minLength(Constant.minLength),
+				Validators.maxLength(Constant.maxLength),
+			]
+		),
+		lastName: new FormControl('',
+			[
+				Validators.required,
+				Validators.minLength(Constant.minLength),
+				Validators.maxLength(Constant.maxLength),
+			]
+		),
+		email: new FormControl('',
+			[
+				Validators.required, 
+				Validators.pattern(Constant.emailRegex)
+			]
+		),
+		phone: new FormControl('', [
+			Validators.required,
+			Validators.pattern(Constant.phoneRegex),]
+		),
+			
+		dob: new FormControl('', 
+			Validators.required
+		),
+		role: new FormControl<string | null>(null, Validators.required),
+		password: new FormControl('', [
+			Validators.required,
+			Validators.minLength(Constant.minPasswordLength),
+			Validators.maxLength(Constant.maxPasswordLength),
+		]),
+		password2: new FormControl('', [
+			Validators.required,
+			Validators.minLength(Constant.minConfirmPasswordLength),
+			Validators.maxLength(Constant.maxConfirmPasswordLength),
+		]),
+	});
+	
 }
