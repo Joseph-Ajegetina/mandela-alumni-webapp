076084475bab3c4fc1a2dcf0a9697ef182b09e94
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-03-12 01:21:48 +0000

    add the componen

---
+++ src/app/features/admin/approval/approval.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TuiCell, TuiHeader, TuiSearch } from '@taiga-ui/layout';
+import { FormControl, FormGroup, FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { TuiButton, TuiTextfield } from '@taiga-ui/core';
+import {TuiTable} from '@taiga-ui/addon-table';
+import data from './models/data';
+import { TuiBadge, TuiCheckbox, TuiStatus } from '@taiga-ui/kit';
+
+@Component({
+	selector: 'app-approval',
+	imports: [CommonModule,FormsModule, ReactiveFormsModule, TuiHeader, TuiCell, TuiSearch, TuiTextfield, TuiButton, TuiTable, TuiStatus, TuiBadge],
+	templateUrl: './approval.component.html',
+	styleUrl: './approval.component.less',
+})
+export class ApprovalComponent {
+	protected size = 'l'
+	tableData = data;
+	search = '';
+	form = new FormGroup({
+		search: new FormControl('')
+	})
+
+	protected get checked(): boolean | null {
+        const every = this.tableData.every(({selected}) => selected);
+        const some = this.tableData.some(({selected}) => selected);
+ 
+        return every || (some && null);
+    }
+ 
+    protected onCheck(checked: boolean): void {
+        this.tableData.forEach((item) => {
+            item.selected = checked;
+        });
+    }
+}
