e1dea6fdcf8291baa3455028eceb0ee10356a1b4
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-04-01 16:09:12 +0000

    create new library to house state and access features

---
+++ libs/core-data/src/lib/core-data/core-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'lib-core-data',
+	imports: [CommonModule],
+	templateUrl: './core-data.component.html',
+	styleUrl: './core-data.component.css',
+})
+export class CoreDataComponent {}
---
+++ libs/core-state/src/lib/core-state/core-state.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'lib-core-state',
+	imports: [CommonModule],
+	templateUrl: './core-state.component.html',
+	styleUrl: './core-state.component.css',
+})
+export class CoreStateComponent {}
