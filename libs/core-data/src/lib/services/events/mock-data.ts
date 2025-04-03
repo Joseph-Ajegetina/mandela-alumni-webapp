186fd006ad9c0aca33efaaf0b13808857c3a2c76
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-04-03 02:46:22 +0000

    add mock data

---
+++ libs/core-data/src/lib/services/events/mock-data.ts
@@ -0,0 +1,40 @@
+import { Event } from '@mandela-alumni-webapp/api-interfaces';
+export const EVENTS: Partial<Event[]> = [
+	{
+		image: 'images/alumracle/slider1.jpg',
+
+		type: 'hybrid',
+		title: 'Career Growth Webinar',
+		description:
+			'In accumsan sit amet quam nec imperdiet. Curabitug gravida eros eget felis pellentesque dictum eu nec est. Quisquet interdum ante velmsan sit amet quam nec imperdie fikhet....',
+		date: new Date('April 04, 2025'), 
+        location: "Ghana",
+        name: 'Ghana meetup',
+        id: '1',
+	},
+
+	{
+		image: 'images/alumracle/slider2.jpg',
+
+		type: 'online',
+		title: 'Career Growth Webinar',
+		description:
+			'In accumsan sit amet quam nec imperdiet. Curabitug gravida eros eget felis pellentesque dictum eu nec est. Quisquet interdum ante velmsan sit amet quam nec imperdie fikhet....',
+		date: new Date('April 04, 2025'),
+        location: "USA",
+        name: 'USA meetup',
+        id: '2',
+	},
+
+	{
+		image: 'images/alumracle/slider3.jpg',
+		type: 'physical',
+		title: 'Career Growth Webinar',
+		description:
+			'In accumsan sit amet quam nec imperdiet. Curabitug gravida eros eget felis pellentesque dictum eu nec est. Quisquet interdum ante velmsan sit amet quam nec imperdie fikhet....',
+		date: new Date('April 04, 2025'),
+        location: "BRITAIN",
+        name: 'Britain meetup',
+        id: '3',
+	},
+];
