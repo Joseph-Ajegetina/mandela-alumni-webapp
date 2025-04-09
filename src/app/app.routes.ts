a6e92707341f99818ad6b67654142762517e6c06
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-04-09 18:25:05 +0000

    restructure preference page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { LoginResolve } from './core/resolvers/login.resolve';
 import { AuthGuard } from './core/guards/auth.guard';
 import { ApprovalPageComponent } from './pending-approval/approval-page/approval-page.component';
 import { AdminGuard } from './core/guards/admin.guard';
-import { PreferencesComponent } from './features/preferences/preferences.component';
 
 export const appRoutes: Route[] = [
 	{
@@ -28,10 +27,6 @@ export const appRoutes: Route[] = [
 	{
 		path: 'register',
 		component: RegisterComponent,
-	},
-	{
-		path: 'preferences',
-		component: PreferencesComponent
 	},
 
 	{
@@ -68,25 +63,33 @@ export const appRoutes: Route[] = [
 	},
 	{
 		path: 'account',
-		loadComponent: ()=>
-			import('./shared/ui/account-layout/account-layout.component').then((m) => m.AccountLayoutComponent),
+		loadComponent: () =>
+			import('./shared/ui/account-layout/account-layout.component').then(
+				(m) => m.AccountLayoutComponent,
+			),
 		children: [
 			{
 				path: 'profile',
-				loadComponent: ()=>
-					import('./account-mangement/features/profile-page/profile-page.component').then((m) =>m.ProfilePageComponent)
+				loadComponent: () =>
+					import('./account-mangement/features/profile-page/profile-page.component').then(
+						(m) => m.ProfilePageComponent,
+					),
 			},
 			{
 				path: 'security',
-				loadComponent: ()=>
-					import('./account-mangement/features/security-access/security-access.component').then((m) =>m.SecurityAccessComponent)
+				loadComponent: () =>
+					import('./account-mangement/features/security-access/security-access.component').then(
+						(m) => m.SecurityAccessComponent,
+					),
 			},
 			{
 				path: 'preference',
-				loadComponent: ()=>
-					import('./account-mangement/features/preference-page/preference-page.component').then((m) =>m.PreferencePageComponent)
+				loadComponent: () =>
+					import('./account-mangement/features/preference-page/preference-page.component').then(
+						(m) => m.PreferencePageComponent,
+					),
 			},
-		]
+		],
 	},
 	{
 		path: '**',
