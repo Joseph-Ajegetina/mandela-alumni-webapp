d67487d873c3ef221e0e7c9f3cdfff0c8dd952ba
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-04-28 03:17:14 +0000

    clean up ui

--- src/app/features/payment-page/payment-page.component.spec.ts
+++ src/app/features/payment-page/payment-page.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PaymentPageComponent } from './payment-page.component';
 
 describe('PaymentPageComponent', () => {
-  let component: PaymentPageComponent;
-  let fixture: ComponentFixture<PaymentPageComponent>;
+	let component: PaymentPageComponent;
+	let fixture: ComponentFixture<PaymentPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PaymentPageComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [PaymentPageComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(PaymentPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(PaymentPageComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
